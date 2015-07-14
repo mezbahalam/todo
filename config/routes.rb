236f0c90cd70afabf70716e9c48d7c0872c0bd9c
mezbahalam <mezbahalam26@gmail.com>
2015-07-15 02:47:14 +0600

    added todo_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+
   root "todo_lists#index"
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
